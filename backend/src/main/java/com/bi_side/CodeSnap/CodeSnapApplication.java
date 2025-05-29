39dc9f296e72626a49704aea861d69facab67cbd
mgsong <mgsong@bumil.co.kr>
2025-05-30 08:40:51 +0900

    초기 설정

---
+++ backend/src/main/java/com/bi_side/CodeSnap/CodeSnapApplication.java
@@ -0,0 +1,13 @@
+package com.bi_side.CodeSnap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeSnapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeSnapApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/bi_side/CodeSnap/CodeSnapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bi_side.CodeSnap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeSnapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
